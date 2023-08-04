d0eb52fb2bfa5e4b580db7696c0b0a60406e9ec4
Simon <simon@7lieues.io>
2023-08-04 16:14:14 +0200

    feat: integrates all components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Charts } from './components/Charts';
+import { Context } from './components/Context';
+import { Results } from './components/Results';
+import { Weather } from './components/Weather';
+import { Footer } from './components/nav/Footer';
+import { Header } from './components/nav/Header';
+import { nestedDataTable } from './helpers/api.helper';
+import { getAllPredictions, saveGroundTruth } from './helpers/db.helper';
+
+export interface prediction {
+  id: number,
+  time_period: string,
+  predicted_value: number,
+  ground_truth: number,
+  created_at: Date
+}
+export const data: prediction[] | undefined = await getAllPredictions();
 
 function App() {
+  async function setGroundTruthWhenAvailable(data: prediction[]) {
+    for (let i = 0; i < data.length; i++) {
+      const [year, quarter] = data[i].time_period.split("-");
+
+      // We check if the nestedDataTable has value for this time period
+      if (nestedDataTable[year][quarter]) saveGroundTruth(data[i].time_period, nestedDataTable[year][quarter])
+    }
+  }
+
+  setGroundTruthWhenAvailable(data!);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper" className="App text-center">
+      <Header />
+
+      <main className="mx-auto">
+        <Weather />
+        <Context />
+        <Charts />
+        <Results />
+      </main>
+
+      <Footer />
     </div>
   );
 }
